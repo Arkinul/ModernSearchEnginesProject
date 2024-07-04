495cce643e7f731ede24ed6e316b72f1c898baa0
xaos <github.com@xaos.tech>
2024-07-04 13:57:14 +0200

    move Index.queue_url into more comprehensive Queue class

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import hashlib
 import re  # regex
-
+import warnings
 from contextlib import contextmanager
 
 #Add a document to the index. You need (at least) two parameters:
@@ -45,6 +45,7 @@ class Index:
         self.con = sqlite3.connect(db, isolation_level = None)
         self.con.execute("PRAGMA foreign_keys = 1")
 
+
     # SQLite works better in autocommit mode when using short DML (INSERT /
     # UPDATE / DELETE) statements
     # credit: https://github.com/litements/litequeue/blob/main/litequeue.py#L568
@@ -63,13 +64,38 @@ class Index:
         else:
             self.con.commit()
 
-    def queue_url(self, url, position):
+
+class Queue(Index):
+    def shift(self, cur, position, amount = 1):
         '''
-        Insert an URL into the `url` table and add it to the frontier at the given position
+        Move all rows in the frontier >= `position` back by `amount`
+
+        Negative `amount`s shift them forward.
+        '''
+        # push forward or back
+        # can't be done in one statement due to this limitation:
+        # https://stackoverflow.com/a/7703239
+        cur.execute(
+            "UPDATE frontier \
+            SET position = -(position + ?2) \
+            WHERE position >= ?1",
+            (position, amount)
+        )
+        cur.execute(
+            "UPDATE frontier \
+            SET position = abs(position) \
+            WHERE position < 0"
+        )
+
+
+    def push(self, url):
+        '''
+        Add an URL to the end of the frontier & create `url` entry if necessary
+
+        Does nothing if the URL is already queued.
         '''
         # TODO: normalize URL
         with self.transaction():
-            print(f"queuing URL: {url}")
             cur = self.con.cursor()
             # check if URL is already in the URL table, also return position if already queued
             id_and_position = cur.execute(
@@ -78,50 +104,106 @@ class Index:
                 WHERE url.url LIKE ?1",
                 (url, )
             ).fetchone()
-            #print(id_and_position)
             if id_and_position:
+                #print(f"URL already stored with id {url_id}")
                 url_id, prev_pos = id_and_position
                 if prev_pos is not None:
-                    print(f"URL already queued at {prev_pos} with id {url_id}")
-                    if position == prev_pos:
-                        return
-                    else:
-                        raise NotImplementedError("TODO: move URL in queue")
-                else:
-                    print(f"URL already stored with id {url_id}")
+                    #print(f"URL already queued at {prev_pos}")
+                    return
             else:
                 # insert URL into url table
-                cur.execute(
+                res = cur.execute(
                     "INSERT OR IGNORE INTO url (url) \
-                    VALUES (?1)",
+                    VALUES (?1) \
+                    RETURNING url.id",
                     (url, )
-                )
-                if cur.rowcount == 1:
-                    url_id = cur.lastrowid
-                    print(f"{url_id}")
-                else:
-                    print(f"failed to store URL {url} in table")
-                    return
+                ).fetchone()
+                assert cur.rowcount == 1, f"failed to store URL {url} in table"
+                (url_id, ) = res
 
-            # push back
-            # can't be done in one statement due to this limitation:
-            # https://stackoverflow.com/a/7703239
-            cur.execute(
-                "UPDATE frontier \
-                SET position = -(position + 1) \
-                WHERE position >= ?1",
-                (position, )
-            )
-            cur.execute(
-                "UPDATE frontier \
-                SET position = abs(position) \
-                WHERE position < 0"
-            )
-
-            # insert frontier entry
+            # insert frontier entry at the end
             cur.execute(
                 "INSERT INTO frontier (position, url_id) \
-                VALUES (?1, ?2)",
+                VALUES ( \
+                    IFNULL((SELECT max(position) + 1 FROM frontier), 0), \
+                    ?1 \
+                )",
+                (url_id, )
+            )
+
+
+    def pop(self):
+        with self.transaction():
+            cur = self.con.cursor()
+            pos_and_url = cur.execute(
+                "DELETE FROM frontier \
+                WHERE position = (SELECT min(position) FROM frontier) \
+                RETURNING position, (SELECT url FROM url WHERE id = url_id)"
+            ).fetchone()
+            if pos_and_url:
+                pos, url = pos_and_url
+                if pos >= 0:
+                    #TODO: optimize by not shifting down after every single pop
+                    self.shift(cur, pos, -1)
+                else:
+                    warnings.warn(f"queue entry with negative position {pos}")
+                return url
+            else:
+                return None
+
+
+    def insert(self, url, position):
+        '''
+        Insert an URL into the `url` table and add it to the frontier at the given position, without creating gaps
+        '''
+        # TODO: normalize URL
+        with self.transaction():
+            cur = self.con.cursor()
+            # check if URL is already in the URL table, also return position if already queued
+            id_and_position = cur.execute(
+                "SELECT id, frontier.position FROM url \
+                FULL OUTER JOIN frontier ON url.id = frontier.url_id \
+                WHERE url.url LIKE ?1",
+                (url, )
+            ).fetchone()
+            if id_and_position:
+                url_id, prev_pos = id_and_position
+                #print(f"URL already stored with id {url_id}")
+                if prev_pos == position:
+                    return
+                elif prev_pos is not None:
+                    #print(f"URL already queued at {prev_pos}")
+                    # take it out of the frontier
+                    cur.execute(
+                        "DELETE FROM frontier WHERE position = ?1",
+                        (prev_pos, )
+                    )
+                    # close the gap
+                    self.shift(cur, prev_pos, -1)
+                    # TODO: optimize by checking if prev_pos < position, only shift once
+            else:
+                # insert URL into url table
+                res = cur.execute(
+                    "INSERT OR IGNORE INTO url (url) \
+                    VALUES (?1) \
+                    RETURNING url.id",
+                    (url, )
+                ).fetchone()
+                assert cur.rowcount == 1, f"failed to store URL {url} in table"
+                (url_id, ) = res
+
+            # make space for the frontier entry
+            self.shift(cur, position)
+            # insert frontier entry into the gap or at the end, but not after
+            cur.execute(
+                "INSERT INTO frontier (position, url_id) \
+                VALUES ( \
+                    MAX(0, MIN( \
+                        ?1, \
+                        IFNULL((SELECT max(position) + 1 FROM frontier), 0) \
+                    )), \
+                    ?2 \
+                )",
                 (position, url_id)
             )
 
@@ -286,9 +368,9 @@ def load_urls(db, urls):
     '''
     Load URLs from file and insert into the frontier
     '''
-    index = Index(db)
+    queue = Queue(db)
     for i, url in enumerate(urls):
-        index.queue_url(url.strip(), i)
+        queue.push(url.strip())
 
 
 if __name__ == '__main__':
