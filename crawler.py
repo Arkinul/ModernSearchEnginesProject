216e1157b87b056104da465261283d6f2d568bb9
Cohen2000 <albert.borchardt@gmail.com>
2024-07-08 00:21:21 +0200

    Implemented respecting of robots.txt and url_host extraction (database conncetion for this needed)

--- crawler.py
+++ crawler.py
@@ -6,6 +6,8 @@ import hashlib
 import re  # regex
 import warnings
 from contextlib import contextmanager
+from urllib.parse import urlparse
+from urllib.robotparser import RobotFileParser
 
 #Add a document to the index. You need (at least) two parameters:
 #doc: The document to be indexed.
@@ -360,6 +362,52 @@ def check_duplicate(page_data):
     #TODO:insert page with new simhash into database
     existing_simhashes.append(simhash)
     return True
+
+
+#TODO: Instead of robots_cache dictonary use database to save and fetch robots.txt
+robots_cache = {}
+
+def get_host(url):
+    '''
+    Extracts the host (scheme and netloc) from the given URL.
+
+    Parameters:
+    url (str): The URL from which the host is to be extracted.
+
+    Returns:
+    str: The host part of the URL, including the scheme.
+    '''
+    parsed_url = urlparse(url)
+    return parsed_url.scheme + "://" + parsed_url.netloc
+
+
+def is_allowed_by_robots(url):
+    '''
+    Determines if the given URL is allowed to be crawled according to the robots.txt rules of the host.
+
+    Parameters:
+    url (str): The URL to check against the robots.txt rules.
+
+    Returns:
+    bool: True if the URL is allowed to be crawled, False otherwise.
+    '''
+    host = get_host(url)
+    if host not in robots_cache:
+        print(f"Fetching robots.txt for {host}")
+        robots_txt_url = host + "/robots.txt"
+        rp = RobotFileParser()
+        rp.set_url(robots_txt_url)
+        rp.read()
+        robots_cache[host] = rp
+
+    rp = robots_cache[host]
+    user_agent = 'MSE_Crawler' 
+    if rp.can_fetch(user_agent, url):
+        return True
+    else:
+        return False
+
+
 
 
 @c.command()
