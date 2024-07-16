1f3f862987d97a2d54af7b5b509d260cb6385469
xaos <github.com@xaos.tech>
2024-07-16 16:42:57 +0200

    rework inverse index schema

--- index.sql
+++ index.sql
@@ -7,13 +7,19 @@ CREATE TABLE IF NOT EXISTS "document" (
 	"content"	TEXT
 );
 
+CREATE TABLE IF NOT EXISTS "word" (
+    "id"    INTEGER NOT NULL PRIMARY KEY,
+    "word"  TEXT NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS "invertedIndex" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
-    "word" TEXT NOT NULL,
-    "document_id" INTEGER,
-    FOREIGN KEY("document_id") REFERENCES "document",
-    "next_pointer" INTEGER,
-    "skip_pointer" INTEGER
+    "word_id" INTEGER NOT NULL,
+    "document_id" INTEGER NOT NULL,
+    PRIMARY KEY("word_id", "document_id"),
+    FOREIGN KEY("word_id") REFERENCES "word",
+    FOREIGN KEY("document_id") REFERENCES "document"
+    --"next_pointer" INTEGER,
+    --"skip_pointer" INTEGER
 );
 
 COMMIT;
